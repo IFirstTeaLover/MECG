1483d2a409d91a036c95d1aa21c338f49406d39c
BulbaSprout <pasabozenko3@gmail.com>
2026-04-18 19:26:58 +0300

    Refactor audio file paths for consistency

--- script/game/audio.js
+++ script/game/audio.js
@@ -1,26 +1,18 @@
 window.audio = {}
-
-const song = "toaster-kubbi.mp3" //change la song here gng
-
-audio.song = new Audio(`../../assets/audio/${song}`) 
-audio.click0 = new Audio(`../../assets/audio/click0.wav`)
-audio.click1 = new Audio(`../../assets/audio/click1.wav`)
-audio.coolclick = new Audio(`../../assets/audio/coolclick.wav`)
-
-audio.death_meow = new Audio(`../../assets/audio/meow-death.mp3`)
-audio.death_meow_abyss = new Audio(`../../assets/audio/meow-death-abyss.mp3`)
-
-audio.splash = new Audio(`../../assets/audio/splash.mp3`)
-
-audio.footstep0 = new Audio(`../../assets/audio/footstep0.wav`)
-audio.footstep1 = new Audio(`../../assets/audio/footstep1.wav`)
-audio.footstep2 = new Audio(`../../assets/audio/footstep2.wav`)
-
-audio.jetpack = new Audio(`../../assets/audio/jetpack.wav`)
-
-audio.trigger = new Audio(`../../assets/audio/trigger.wav`)
-
-audio.pickup = new Audio(`../../assets/audio/pickup.ogg`)
+const song = "toaster-kubbi.mp3"
+audio.song = new Audio(`./assets/audio/${song}`)
+audio.click0 = new Audio(`./assets/audio/click0.wav`)
+audio.click1 = new Audio(`./assets/audio/click1.wav`)
+audio.coolclick = new Audio(`./assets/audio/coolclick.wav`)
+audio.death_meow = new Audio(`./assets/audio/meow-death.mp3`)
+audio.death_meow_abyss = new Audio(`./assets/audio/meow-death-abyss.mp3`)
+audio.splash = new Audio(`./assets/audio/splash.mp3`)
+audio.footstep0 = new Audio(`./assets/audio/footstep0.wav`)
+audio.footstep1 = new Audio(`./assets/audio/footstep1.wav`)
+audio.footstep2 = new Audio(`./assets/audio/footstep2.wav`)
+audio.jetpack = new Audio(`./assets/audio/jetpack.wav`)
+audio.trigger = new Audio(`./assets/audio/trigger.wav`)
+audio.pickup = new Audio(`./assets/audio/pickup.ogg`)
 
 
 
